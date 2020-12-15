b0eac81ce29b5722dc6d3d5862735f4ccd49d8fe
Kenpil <hdmiken@gmail.com>
2020-12-16 03:19:32 +0900

    tired!

---
+++ build/imuTest/CMakeFiles/mpuDataListenerTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpuDataListenerTest.dir/src/mpuDataListenerTest.cpp.o"
+  "/home/ken/ubuROBOTS/develop/IMU_ws/devel/lib/imuTest/mpuDataListenerTest.pdb"
+  "/home/ken/ubuROBOTS/develop/IMU_ws/devel/lib/imuTest/mpuDataListenerTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mpuDataListenerTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/imuTest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ken/ubuROBOTS/develop/IMU_ws/src/imuTest
+# Build directory: /home/ken/ubuROBOTS/develop/IMU_ws/build/imuTest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
